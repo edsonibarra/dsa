85d05d4dc24a5371593584393eeb43d81f892ed1
edsonibarra <edsonibarra97@gmail.com>
2023-01-28 16:12:29 -0600

    append method

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,16 @@
+from node import Node
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
